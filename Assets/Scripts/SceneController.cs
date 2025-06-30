f14d8312e4e7b48eba5abc5ccd3ca563d3f90eb7
narlon <narlonzjh@163.com>
2025-06-30 22:05:56 +0800

    init

---
+++ Assets/Scripts/SceneController.cs
@@ -0,0 +1,22 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using TMPro; // 引入 TextMeshPro 命名空间
+
+public class SceneController : MonoBehaviour
+{
+    public GameObject Plane;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+    }
+
+}
